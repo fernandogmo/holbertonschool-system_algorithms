4622ddfbf9829fdae619d0892650cb0d28251343
Fernando Gonzalez-Morales <fernando.gonzalez@holbertonschool.com>
2020-10-22 20:04:27 -0700

    Moved tree rotation functions

--- 0x00-red_black_tree/0-rb_tree_node.c
+++ 0x00-red_black_tree/0-rb_tree_node.c
@@ -20,3 +20,61 @@ rb_tree_t *rb_tree_node(rb_tree_t *parent, int value, rb_color_t color)
 
 	return (new);
 }
+
+
+/* These functions will be used for task 2 and task 4 */
+/**
+ * rotr - rotates subtree right
+ * @tree: double pointer to root node
+ * @node: pointer to node where violation detected
+ *
+ * child is the pivot point, that is the link between the newly inserted node
+ * and its grandparent
+ */
+void rotr(rb_tree_t **tree, rb_tree_t *node)
+{
+	rb_tree_t *child = node->left;
+
+	if (!child)
+		return;
+	node->left = child->right;
+	if (child->right)
+		child->right->parent = node;
+	child->parent = node->parent;
+	if (!node->parent)
+		*tree = child;
+	else if (node == node->parent->right)
+		node->parent->right = child;
+	else
+		node->parent->left = child;
+	child->right = node;
+	node->parent = child;
+}
+
+/**
+ * rotl - rotates subtree left
+ * @tree: double pointer to root node
+ * @node: pointer to node where violation detected
+ *
+ * child is the pivot point, that is the link between the newly inserted node
+ * and its grandparent
+ */
+void rotl(rb_tree_t **tree, rb_tree_t *node)
+{
+	rb_tree_t *child = node->right;
+
+	if (!child)
+		return;
+	node->right = child->left;
+	if (child->left)
+		child->left->parent = node;
+	child->parent = node->parent;
+	if (!node->parent)
+		*tree = child;
+	else if (node == node->parent->left)
+		node->parent->left = child;
+	else
+		node->parent->right = child;
+	child->left = node;
+	node->parent = child;
+}
