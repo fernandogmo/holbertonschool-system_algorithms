f2a29c2ab3c1fc1665198f4169a072f573b14b11
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-04-14 22:04:28 -0700

    Task 1: tracking previous vertex for inserting new vertex

--- 0x01-graphs/1-graph_add_vertex.c
+++ 0x01-graphs/1-graph_add_vertex.c
@@ -9,18 +9,18 @@
 vertex_t *graph_add_vertex(graph_t *graph, const char *str)
 {
 	vertex_t *v = calloc(1, sizeof(vertex_t));
-	vertex_t *p = graph ? graph->vertices : NULL;
+	vertex_t *prev, *curr = graph ? graph->vertices : NULL;
 
 	if (!graph || !str || !*str || !v)
 		goto fail;
-	if (!p)
+	if (!curr)
 		graph->vertices = v;
 	else
 	{
-		for (; p->next; p = p->next)
-			if (!strcmp(p->content, str))
+		for (prev = curr; curr; prev = curr, curr = curr->next)
+			if (!strcmp(curr->content, str))
 				goto fail;
-		p->next = v;
+		prev->next = v;
 	}
 	v->index = graph->nb_vertices++;
 	v->content = strdup(str);
