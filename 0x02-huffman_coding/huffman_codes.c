991f654e6ccf2492f1a70c03e27ed0ffecfd965c
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-05-06 23:57:39 -0700

    Valgrind issues

---
+++ 0x02-huffman_coding/huffman_codes.c
@@ -0,0 +1,13 @@
+#include "huffman.h"
+
+/**
+ * huffman_codes - description
+ * @data: data
+ * @freq: freq
+ * @size: size
+ * Return: int
+ */
+int huffman_codes(char *data, size_t *freq, size_t size)
+{
+	return (-1);
+}
---
+++ 0x02-huffman_coding/huffman_extract_and_insert.c
@@ -0,0 +1,11 @@
+#include "huffman.h"
+
+/**
+ * huffman_extract_and_insert - description
+ * @priority_queue: heap_t
+ * Return: int
+ */
+int huffman_extract_and_insert(heap_t *priority_queue)
+{
+	return (-1);
+}
