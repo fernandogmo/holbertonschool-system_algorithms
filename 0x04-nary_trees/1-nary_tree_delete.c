22c55e68f983fd892fbe3ca06d0607634ba41837
Fernando Gonzalez-Morales <fern@ndo.ai>
2021-07-13 23:38:26 -0700

    Nary tree solutions

---
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "nary_trees.h"
+
+/**
+ * nary_tree_delete - frees all nodes in a nary_tree_t
+ * @tree: pointer to nary_tree_t to be freed
+ */
+void nary_tree_delete(nary_tree_t *tree)
+{
+	while (tree && tree->children)
+	{
+		nary_tree_t *node = tree->children;
+		tree->children = node->next;
+		nary_tree_delete(node);
+	}
+	free(tree->content);
+	free(tree);
+}
